89acac3cdb0b2d5c49f141ea10d52c4323b68f50
Grigory-RobinGood <grigoriy1986biz@mail.ru>
2024-11-22 23:07:05 +0300

    Update crud_function.py

--- crud_function.py
+++ crud_function.py
@@ -2,7 +2,9 @@ import sqlite3
 
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
-#cursor.execute("DROP TABLE IF EXISTS Products")
+
+
+#cursor.execute("DROP TABLE IF EXISTS Users")
 
 
 def initiate_db():
@@ -11,11 +13,31 @@ def initiate_db():
     id INT PRIMARY KEY NOT NULL,
     title TEXT,
     description TEXT NOT NULL,
-    price INT
-    )
+    price INT)
+    ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL)
     ''')
     connection.commit()
-    connection.close()
+
+
+def add_user(username, email, age):
+    cursor.execute(f"INSERT INTO Users (username, email, age, balance) VALUES('{username}', '{email}', '{age}', 1000)")
+    connection.commit()
+    return
+
+
+def is_included(username):
+    check_user = cursor.execute(f"SELECT * FROM Users WHERE username=?", (username,))
+    if check_user.fetchall() is None:
+        return False
+    else:
+        return True
 
 
 def get_all_products():
